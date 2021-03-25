db1e6ff33f57f4cc4487ec99f374d414d9243769
fixCastro <37239241+fixCastro@users.noreply.github.com>
2021-03-25 02:29:53 -0300

    Password Generator
    Added a password generator with private results on Discord DM.

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 #pip install googletrans==4.0.0-rc1
 from googletrans import Translator
+import string, random
 
 class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(aliases=['t'])
+    @commands.command(aliases=['t', 'traduzir'])
     async def translate(self, ctx, lang, *, args):
         t = Translator()
         content = t.translate(str(args), dest=lang)
@@ -15,10 +16,34 @@ class Utilities(commands.Cog):
             await ctx.send(f'```ini\n[{content.text} - {content.pronunciation}]\n```')
         else:
             await ctx.send(f'```ini\n[{content.text}]\n```')
+    
+    @commands.command(
+        aliases=['senha', 'pw'], 
+        description='Gerador de senha, utilize o comando e a quantidade de caracteres desejada.')
+    async def password(self, ctx, length):
+        channel = await ctx.message.author.create_dm()
+        digits = string.ascii_letters + string.digits
+        punctuation = string.punctuation
+        result = ''
+        for _ in range(int(length)):
+            result += random.choice(digits)
+        result += random.choice(punctuation)
+        try:
+            await channel.send(f'```ini\nSenha gerada com sucesso: {result}\n```')
+        except:
+            await channel.send(f'```ini\nErro ao gerar a senha, tente novamente.\n```')
 
     @translate.before_invoke
     async def typing(self, ctx):
         await ctx.trigger_typing()
+    
+    @password.before_invoke
+    async def delete(self, ctx):
+        channel = ctx.channel
+        try:
+            await channel.purge(limit=1)
+        except:
+            pass
 
 def setup(bot):
     bot.add_cog(Utilities(bot))
